ff75b2c61a9a264349600cf694d2aa269a83d8dd
Shitakami <vxd.naoshi.19961205.maro@gmail.com>
2024-09-29 23:06:37 +0900

    [script] アニメーションで使用するScriptableObjectを定義

---
+++ URP-GPUInstancing-Sample/Assets/Scripts/AnimationSettings/CubeAnimationSettings.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace AnimationSettings
+{
+    [CreateAssetMenu(fileName = "AnimationSettings", menuName = "AnimationSettings", order = 0)]
+    public class CubeAnimationSettings : ScriptableObject
+    {
+        [SerializeField] private float _moveVelocity;
+        [SerializeField] private float _rotationVelocity;
+
+        public float MoveVelocity => _moveVelocity;
+        public float RotationVelocity => _rotationVelocity;
+    }
+}
